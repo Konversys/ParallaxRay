e4d7f6d2ba21e2cf88a80d1202cf71f35f482f54
Халилов Рустам - LN <konversys@gmail.com>
2019-06-09 01:31:17 +0700

    Добавлена навигация

--- app/src/main/java/ru/konverdev/parallax/MainActivity.java
+++ app/src/main/java/ru/konverdev/parallax/MainActivity.java
@@ -1,11 +1,21 @@
 package ru.konverdev.parallax;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
@@ -21,6 +31,8 @@ import ru.konverdev.parallax.utils.web.ApiConnector;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar actionBar;
+    private Toolbar toolbar;
     private static FragmentManager fragmentManager;
 
     @Override
@@ -28,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Realm.init(this);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        initNavigationMenu();
         fragmentManager = getSupportFragmentManager();
         if (Direction.GetDirections(0).isEmpty()){
             FragmentHandler.Download(fragmentManager, R.id.AcMainFrame);
@@ -44,5 +58,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle("Расписание");
+    }
+
+    private void initNavigationMenu() {
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem item) {
+                Toast.makeText(getApplicationContext(), item.getTitle() + " Selected", Toast.LENGTH_SHORT).show();
+                actionBar.setTitle(item.getTitle());
+                drawer.closeDrawers();
+                return true;
+            }
+        });
+
+        // open drawer at start
+        drawer.openDrawer(GravityCompat.START);
     }
 }
